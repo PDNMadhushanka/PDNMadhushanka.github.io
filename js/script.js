dd74dc43d25532e20c24a5287c47d996baf1c5b3
PDNMadhushanka <nishanmadhushanka821@gmail.com>
2025-05-21 01:00:31 +0530

    Development completed

--- js/script.js
+++ js/script.js
@@ -1,9 +1,38 @@
 const navLinks = document.querySelectorAll('header nav a');
+const logoLink = document.querySelector('.logo');
+const sections = document.querySelectorAll('section');
+const menuIcon = document.querySelector('#menu-icon');
+const navbar = document.querySelector('header nav');
+
+menuIcon.addEventListener('click', () => {
+    menuIcon.classList.toggle('fa-xmark');
+    navbar.classList.toggle('active');
+});
 
 const activePage = () => {
+    const header = document.querySelector('header');
+    const barsBox = document.querySelector('.bars-box');
+
+    header.classList.remove('active');
+    setTimeout(() => {
+        header.classList.add('active');
+    }, 1100);
+
     navLinks.forEach(link => {
         link.classList.remove('active');
     });
+
+    barsBox.classList.remove('active');
+    setTimeout(() => {
+        barsBox.classList.add('active');
+    }, 1100);
+
+    sections.forEach(section => {
+        section.classList.remove('active');
+    });
+
+    menuIcon.classList.remove('fa-xmark');
+    navbar.classList.remove('active');
 }
 
 navLinks.forEach((link, index) => {
@@ -12,8 +41,24 @@ navLinks.forEach((link, index) => {
             activePage();
 
             link.classList.add('active');
+
+            setTimeout(() => {
+                sections[index].classList.add('active');
+            }, 1100);
         }
     });
+});
+
+logoLink.addEventListener('click', () => {
+    if(!navLinks[0].classList.contains('active')) {
+        activePage();
+
+        navLinks[0].classList.add('active');
+
+        setTimeout(() => {
+            sections[0].classList.add('active');
+        }, 1100);
+    }
 });
 
 const resumeBtns = document.querySelectorAll('.resume-btn');
